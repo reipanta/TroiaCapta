9cd0b51057dac1201a33233e4a43901bce7c687e
Anton Potapov <tauta.rei@gmail.com>
2024-06-30 01:04:25 +0400

    Setup of game state machine

---
+++ TestAssignment_PotionCraft/Assets/Scripts/Infrastructure/GameStateMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure
+{
+    public class GameStateMachine
+    {
+        private readonly Dictionary<Type, IState> _states;
+        private IState _activeState;
+
+        public GameStateMachine()
+        {
+            _states = new Dictionary<Type, IState>();
+        }
+
+        public void EntryPoint<TState>() where TState : IState
+        {
+            _activeState?.Exit();
+            IState _state = _states[typeof(IState)];
+            _activeState = _state;
+            _state.Enter();
+        }
+    }
+}
